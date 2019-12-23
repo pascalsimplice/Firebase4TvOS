768d4827d61e96919901f643061f2789ccb8b9f5
Pascal Simplice <pascal@ayoupa.com>
2019-12-23 22:36:52 +0000

    Test

---
+++ Firebase_tvOS/PublicHeader.h
@@ -0,0 +1,14 @@
+//
+//  PublicHeader.h
+//  Firebase4TvOS
+//
+//  Created by Pascal Simplice on 23/12/2019.
+//  Copyright © 2019 Pascal Simplice. All rights reserved.
+//
+
+#ifndef PublicHeader_h
+#define PublicHeader_h
+
+#import <FirebaseCore/FirebaseCore.h>
+
+#endif /* PublicHeader_h */
